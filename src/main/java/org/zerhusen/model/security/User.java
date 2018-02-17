43acc8f0f6bad2ae10a9bd35fc86dbf975d0af18
arditsarja <arditsarja@gmail.com>
2018-02-17 13:38:54 +0100

    Test

--- src/main/java/org/zerhusen/model/security/User.java
+++ src/main/java/org/zerhusen/model/security/User.java
@@ -20,47 +20,31 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table(name = "USER")
 public class User {
 
     @Id
-    @Column(name = "ID")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
-    @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1)
+    @GeneratedValue
+
     private Long id;
 
-    @Column(name = "USERNAME", length = 50, unique = true)
-    @NotNull
-    @Size(min = 4, max = 50)
+
     private String username;
 
-    @Column(name = "PASSWORD", length = 100)
-    @NotNull
-    @Size(min = 4, max = 100)
+
     private String password;
 
-    @Column(name = "FIRSTNAME", length = 50)
-    @NotNull
-    @Size(min = 4, max = 50)
     private String firstname;
 
-    @Column(name = "LASTNAME", length = 50)
-    @NotNull
-    @Size(min = 4, max = 50)
+
     private String lastname;
 
-    @Column(name = "EMAIL", length = 50)
-    @NotNull
-    @Size(min = 4, max = 50)
+
     private String email;
 
-    @Column(name = "ENABLED")
-    @NotNull
+
     private Boolean enabled;
 
-    @Column(name = "LASTPASSWORDRESETDATE")
-    @Temporal(TemporalType.TIMESTAMP)
-    @NotNull
+
     private Date lastPasswordResetDate;
 
     @ManyToMany(fetch = FetchType.EAGER)
